fdf72009cd7752c429ce6c0982f2b08a6f8a25fe
Jamie Woods <jamie.woods@bbc.co.uk>
2018-04-27 21:49:10 +0100

    Initial working commit

---
+++ docker/dash/start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#     - INGEST_NAME=<%= mount['name'] %>
+#     - INGEST_PROCESSED=<%= mount['processed'] ? 1 : 0 %>
+
+#     - DASH_BITRATE=128,256
+#     - DASH_SEGLENGTH=1000
+
+echo "" > /dashcast.conf
+
+for i in $(echo $DASH_BITRATE | sed "s/,/ /g"); do
+
+	echo "[a${i}]" >> /dashcast.conf
+	echo "type=audio" >> /dashcast.conf
+	echo "bitrate=${i}000" >> /dashcast.conf
+
+done
+
+cat /dashcast.conf
+
+if [ "$INGEST_PROCESSED" == "1" ]; then
+	URI="http://icecast:8000/internal/master/${INGEST_NAME}_processed.flac"
+else
+	URI="http://icecast:8000/internal/master/${INGEST_NAME}.flac"
+fi
+
+while [ true ]; do
+
+	/usr/bin/DashCast -a "http://stream.cor.insanityradio.com/insanity320.mp3" -live -conf /dashcast.conf -out /srv/dash -seg-dur 1000 -frag-dur 1000 -mpd ${INGEST_NAME}.mpd
+
+	sleep 0.1
+done
+
