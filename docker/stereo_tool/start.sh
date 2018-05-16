20707aa901a5fc703205b6933d575886ecb875ba
Jamie Woods <jamie.woods@bbc.co.uk>
2018-05-16 14:22:09 +0100

    Works when using FLAC container with DSP. If we use OGG, it sounds glitchy for some reason

--- docker/stereo_tool/start.sh
+++ docker/stereo_tool/start.sh
@@ -3,7 +3,7 @@
 
 while [ true ]; do
 	ffmpeg -i http://icecast:8000/internal/master/${INGEST_NAME}.flac -f wav -acodec pcm_s16le -ac 2 - | /usr/bin/stereo_tool_cmd - - -s /config.sts | ffmpeg -i - \
-		-c:a flac -compression_level 0 -f ogg -legacy_icecast 1 -content_type audio/flac icecast://source:${AUTH}@icecast:8000/internal/master/${INGEST_NAME}_processed.flac
+		-f flac -legacy_icecast 1 -content_type audio/flac icecast://source:${AUTH}@icecast:8000/internal/master/${INGEST_NAME}_processed.flac
 
 	sleep 0.1
 done
