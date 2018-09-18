c3cc1a34997c6f638cfece47ad6ef4ce73f0314c
docker0 <computing@insanityradio.com>
2018-09-18 01:06:38 +0100

    Make fixes as used in production

--- docker/stereo_tool/start.sh
+++ docker/stereo_tool/start.sh
@@ -3,8 +3,8 @@
 sleep 0.5
 
 while [ true ]; do
-	ffmpeg -i http://icecast:8000/internal/master/${INGEST_NAME}.flac -f wav -acodec pcm_s16le -ac 2 - | /usr/bin/stereo_tool_cmd - - -s /config.sts | ffmpeg -i - \
-		-c:a flac -compression_level 0 -f ogg \
+	ffmpeg -re -i http://icecast:8000/internal/master/${INGEST_NAME}.flac -f wav -acodec pcm_s16le -ac 2 - | /usr/bin/stereo_tool_cmd - - -s /config.sts | ffmpeg -re -i - \
+		-c:a flac -f ogg \
 		-legacy_icecast 1 -content_type audio/ogg icecast://source:${AUTH}@icecast:8000/internal/master/${INGEST_NAME}_processed.flac
 
 	sleep 0.1
